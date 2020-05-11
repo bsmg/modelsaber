dba4cf5cafdef85e321bb2e1e9c8abe9d0ab8eaa
Parapass <parapasser@gmail.com>
2020-05-11 09:53:38 -0400

    fix funny typo

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ require_once 'resources/includes/constants.php';
     <div class="content">
       <h3 class="title is-3 has-text-centered" style='margin-left:0;'>Welcome to <?= SITECAMEL ?>!</h3>
       <p><?= SITECAMEL ?> is a repository for <?= ucfirst(AVATARS) ?>, <?= ucfirst(SABERS) ?>, <?= ucfirst(PLATFORMS) ?>, and <?= ucfirst(NOTES) ?>.</p>
-      <p>If you have the <a href="<?= MODINSTALLER['link'] ?>"><?= MODINSTALLER['name'] ?></a> installed, you can simply click on the <kbd>Install</kbd> buttons to automatically install these models.</p>
+      <p>If you have <a href="<?= MODINSTALLER['link'] ?>"><?= MODINSTALLER['name'] ?></a> installed, you can simply click on the <kbd>Install</kbd> buttons to automatically install these models.</p>
       <p>If you don't, you can still <kbd>Download</kbd> them manually.</p>
       <p>Now with a <strong><em>dark secret...</em></strong></p>
       <p>If you want to learn to make your own, visit the guides for <a href="https://bs.assistant.moe/Avatars"><?= ucfirst(AVATARS) ?></a>, <a href="https://bs.assistant.moe/Sabers"><?= ucfirst(SABERS) ?></a>, <a href="https://bs.assistant.moe/Platforms"><?= ucfirst(PLATFORMS) ?></a>, and <a href="https://bs.assistant.moe/Bloqs"><?= ucfirst(NOTES) ?></a>.</p>
